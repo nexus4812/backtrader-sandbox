0984977d2951d467ebebb89a0b0fef6e01037b50
nexus4812 <powerbatteryboy@gmail.com>
2024-08-10 16:34:29 +0900

    wip

--- strategies/donchian_channel_and_rsi.py
+++ strategies/donchian_channel_and_rsi.py
@@ -1,37 +1,48 @@
-from .base_strategy import BaseStrategy
 import backtrader as bt
 
-class DonchianChannelRSIStrategy(BaseStrategy):
+class DonchianChannelRSIStrategy(bt.Strategy):
     params = (
-        ('profit_period', 180),
-        ('upper_period', 120),
-        ('lower_period', 60),
         ('rsi_period', 14),
-        ('rsi_overbought', 70),
         ('rsi_oversold', 30),
+        ('donchian_period', 20),
+        ('atr_period', 14),  # ATRの期間
     )
 
     def __init__(self):
-        super().__init__(self.params)
-        self.dataclose = self.datas[0].close
-        self.profit_band = bt.indicators.Highest(self.data.high(-1), period=self.params.profit_period)
-        self.upper_band = bt.indicators.Highest(self.data.high(-1), period=self.params.upper_period)
-        self.lower_band = bt.indicators.Lowest(self.data.low(-1), period=self.params.lower_period)
-        self.rsi = bt.indicators.RSI(period=self.params.rsi_period)
+        self.rsi = bt.indicators.RSI(self.data.close, period=self.params.rsi_period)
+
+        self.donchian_low = bt.indicators.Lowest(self.data.low, period=self.params.donchian_period)
+        self.donchian_low.plotinfo.subplot = False
+        self.donchian_low.plotinfo.plotlinelabels = True
+
+        self.atr = bt.indicators.ATR(self.data, period=self.params.atr_period)
+
+        self.entry_price = None  # To track the entry price
 
     def next(self):
         if not self.position:
-            if self.dataclose[0] > self.upper_band[0] and self.rsi[0] < self.params.rsi_overbought:
+            if self.rsi[0] < self.params.rsi_oversold:
                 self.buy()
+                self.entry_price = self.data.close[0]
         else:
-            if self.dataclose[0] < self.lower_band[0] or self.rsi[0] > self.params.rsi_overbought:
-                self.sell()
+            stop_price = max(self.donchian_low[0], self.entry_price - self.atr[0])
+            if self.data.close[0] < stop_price:
+                self.close()
+
+    # @staticmethod
+    # def get_optimization_params():
+    #     return {
+    #         'rsi_period': range(7, 21, 7),
+    #         'rsi_oversold': range(10, 40, 10),
+    #         'donchian_period': range(10, 120, 10),
+    #         'atr_period': range(7, 21, 7),
+    #     }
 
     @staticmethod
     def get_optimization_params():
         return {
-            'upper_period': range(100, 140, 10),
-            'lower_period': range(50, 70, 10),
-            'rsi_period': range(7, 14, 7),
-            'rsi_overbought': range(65, 75, 5),
+            'rsi_period': [7],
+            'rsi_oversold': [30],
+            'donchian_period': [30],
+            'atr_period': [7],
         }
